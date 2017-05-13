418662eb05dbb61de2d5b91a62f024412d8a7ac1
José Aguirre <jaguirre@kreativent.com>
2017-05-13 14:12:07 -0600

    Obtener datos de un archivo json, polilinea en mapa

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
-# usar pyodbc => sql server
+import json
+
 app = Flask(__name__)
 
 
@@ -8,25 +9,20 @@ def index():
     return render_template("index.html")
 
 
-# API: Endpoint - GET
 @app.route("/api/routes")
 def show_routes():
-    # consulta y manejar una estructura de lista -> diccionario
-    routes = [
-        {"lat": -10.0464108, "lng": -74.7593884},
-        {"lat": -10.0464108, "lng": -74.7593884},
-        {"lat": -10.0464108, "lng": -74.7593884},
-        {"lat": -10.0464108, "lng": -74.7593884},
-        {"lat": -10.0464108, "lng": -74.7593884},
-    ]
 
-    return jsonify({"routes": routes})
+    data = open("data.json").read()
+    json_data = json.loads(data)
 
-# curl -i -X POST localhost:5000/routes
+    return jsonify(json_data)
+
+
 @app.route("/api/routes", methods=["POST"])
 def update_routes():
     print(request.form)
     return "Rutas actualizadas"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
